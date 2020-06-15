79b0ed9cff131a1d9c7169fbec5f41a31329a710
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-15 01:43:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -29,8 +29,8 @@ end
 
   def count_sentences
    new_count = self.split(".")
-   new_count.count = other_count
-   other_count.length 
+   new_count.count
+   other_count.length = new_count
   end
  
   
